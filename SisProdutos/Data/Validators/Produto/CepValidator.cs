47804968c1515baa086bd95ba60c066c833fca22
marconapppo <gabrielmarconatto17@gmail.com>
2021-10-27 16:30:41 -0300

    validation instalado com sucesso

---
+++ SisProdutos/Data/Validators/Produto/CepValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SisClientes
+{
+    public class CidadeCepValidator : AbstractValidator<CidadeCep>
+    {
+        public CidadeCepValidator()
+        {
+            RuleFor(x => x.Cep).NotEmpty().WithMessage("O campo cep é obrigatório");
+            RuleFor(x => x.Cep).Matches(@"^[0-9]{5}-?[0-9]{3}$").WithMessage("O campo Cep é inválido");
+            RuleFor(x => x.Logradouro).NotEmpty()
+                .WithMessage("O campo logradouro é obrigatório");
+            RuleFor(x => x.Bairro).NotEmpty()
+                .WithMessage("O campo bairro é obrigatório");
+        }
+    }
+}
---
+++ SisProdutos/Data/Validators/Produto/CreateProdutoValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SisProdutos
+{
+    public class CreateProdutoDto
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public float Preco { get; set; }
+        [Required]
+        public string PalavraChave { get; set; }
+        [Required]
+        public string Categoria { get; set; }
+        [Required]
+        public List<string> CepOpcionais { get; set; }
+    }
+}
