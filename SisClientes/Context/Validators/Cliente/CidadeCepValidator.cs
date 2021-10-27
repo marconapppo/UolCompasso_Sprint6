ce67616840ff32f5821c352dee8600e51069bb29
marconapppo <gabrielmarconatto17@gmail.com>
2021-10-27 16:04:40 -0300

    lista de cep de cliente validado com sucesso

--- SisClientes/Context/Validators/Cliente/CidadeCepValidator.cs
+++ SisClientes/Context/Validators/Cliente/CidadeCepValidator.cs
@@ -5,20 +5,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SisClientes
 {
-    public class CreateClienteValidator : AbstractValidator<CreateClienteDTO>
+    public class CidadeCepValidator : AbstractValidator<CidadeCep>
     {
-        public CreateClienteValidator()
+        public CidadeCepValidator()
         {
-            RuleFor(x => x.Nome).NotEmpty().WithMessage("O campo nome é obrigatório");
-            RuleFor(x => x.DataNascimento).NotEmpty().WithMessage("O campo data de nascimento é obrigatório");
             RuleFor(x => x.Cep).NotEmpty().WithMessage("O campo cep é obrigatório");
             RuleFor(x => x.Cep).Matches(@"^[0-9]{5}-?[0-9]{3}$").WithMessage("O campo Cep é inválido");
             RuleFor(x => x.Logradouro).NotEmpty()
                 .WithMessage("O campo logradouro é obrigatório");
             RuleFor(x => x.Bairro).NotEmpty()
                 .WithMessage("O campo bairro é obrigatório");
-            RuleFor(x => x.CepOpcionais).NotEmpty().WithMessage("O Campo CidadeDic é obrigatório");
-            RuleForEach(x => x.CepOpcionais).SetValidator(new CidadeCepValidator());
         }
     }
 }
